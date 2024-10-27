eda44ec9167e3bba3d62b9bd99042e0ed4c6c8c9
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-10-27 02:41:56 -0300

    Migrations

--- backend/infraestructure/Persistence/Migrations/20241027053336_FirstMigration.Designer.cs
+++ backend/infraestructure/Persistence/Migrations/20241027053336_FirstMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using infraestructure.Persistence;
@@ -11,9 +12,11 @@ using infraestructure.Persistence;
 namespace infraestructure.Persistence.Migrations
 {
     [DbContext(typeof(RecetarioDbContext))]
-    partial class RecetarioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241027053336_FirstMigration")]
+    partial class FirstMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
