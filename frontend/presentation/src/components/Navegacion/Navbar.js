80ad8ca14ebd4dff0c4b106b02c7d7cbe3a2979a
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-10-31 16:28:16 -0300

    React-Bootstrap

--- frontend/presentation/src/components/Navegacion/Navbar.js
+++ frontend/presentation/src/components/Navegacion/Navbar.js
@@ -1,34 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-const Navbar = () => {
-  return (
-    <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <Link to='/'>
-                <img src='./logo.png' width='50'/>
-                </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mx-auto"> 
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/' >Inicio</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/Contacto' >Contacto</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/' >Algo mas</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>    
-    </div>
-  )
-}
-//<ul className="navbar-nav mx-auto"> le agregue el "mx-auto" para centrar los elementos (juguetito de bootstrap)
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Cookie, PersonCircle } from 'react-bootstrap-icons';
 
-export default Navbar
+function CollapsibleExample() {
+    return (
+        <Navbar variant='dark' className='bg-dark' collapseOnSelect expand="lg">
+            <Container>
+
+                <Navbar.Brand href="#home">
+                    <Cookie size={30} />
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className='me-auto' />
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            <PersonCircle size={30} />
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+
+            </Container>
+        </Navbar>
+    );
+}
+
+export default CollapsibleExample;
