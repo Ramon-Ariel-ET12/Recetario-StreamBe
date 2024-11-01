340602247cfe12ce799318fb892ace8e74a53d2e
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-11-01 17:38:29 -0300

    Test

--- frontend/presentation/src/components/Views/Inicio.js
+++ frontend/presentation/src/components/Views/Inicio.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Inicio = () => {
+  let element = [];
+  for (let index = 0; index < 20; index++) {
+    element.push(<h1>Esta es la pagina de Inicio</h1>);
+    
+  }
   return (
     <div>
-        <h1>Esta es la pagina de Inicio</h1>
+      {element}
     </div>
   )
 }
