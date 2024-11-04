fe6bae0b019d3d18772e64f40366c57f50c850e9
NoeliaAT <noelia.almaraz.et12@gmail.com>
2024-11-04 00:43:22 -0300

    Cards

--- frontend/presentation/src/App.js
+++ frontend/presentation/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import './components/Navegacion/Cards.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/Navegacion/Navbar'
 import Inicio from './components/Views/Inicio'
 import Contacto from './components/Views/Contacto'
+import Cards from './components/Navegacion/Cards'
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           {/* y asi para agregar mas elementos,  que pro*/}
         </Routes>
       </Router>
+      <Cards />
     </div>
   );
 }
