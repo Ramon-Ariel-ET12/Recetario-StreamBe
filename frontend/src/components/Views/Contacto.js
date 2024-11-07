3f7de35eee98ffe4e926f3605e468a6b6879b33f
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-11-07 02:24:05 -0300

    Prueba del get hacia el backend

--- frontend/src/components/Views/Contacto.js
+++ frontend/src/components/Views/Contacto.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { jwtDecode } from "jwt-decode"
 
 const Contacto = () => {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        var token = localStorage.getItem("AuthToken");
+        const decodedToken = jwtDecode(token);
+        const id = decodedToken.IdUsuario;
+        console.log(id);
+
+        const response = axios.get(`https://backend-streambe.onrender.com/api/Usuario/Traerusuario?id=${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        console.log(response);
+
+      } catch (error) {
+        console.log(error);
+
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <div>
-        <h1>Esta es la pagina de Contacto</h1>
+      <h1>Esta es la pagina de Contacto</h1>
     </div>
   )
 }
