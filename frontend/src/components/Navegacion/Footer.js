bddf77513d1bc4e2587f2c773d62b4fc37660f88
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-11-08 23:44:32 -0300

    Footer responsivo

--- frontend/src/components/Navegacion/Footer.js
+++ frontend/src/components/Navegacion/Footer.js
@@ -4,19 +4,19 @@ import { useLocation } from "react-router-dom";
 
 function Footer() {
     const { logueado } = useContext(AuthContext);
-    const [message, setMessage] = useState('')
+    const [message, setMessage] = useState('');
     const location = useLocation();
     useEffect(() => {
         if (logueado && location.pathname.toLowerCase() !== "/terminos-y-condiciones") {
-            setMessage(<a href="/Terminos-y-condiciones" target="_blank" >Ver términos y condiciones</a>);
+            setMessage(<a href="/Terminos-y-condiciones" target="_blank">Ver términos y condiciones</a>);
         }
     }, [logueado, location.pathname]);
     return (
         <footer>
-            <nav className="navbar navbar-expand-lg justify-content-between px-4">
-                <a href="https://github.com/Ramon-Ariel-ET12/Recetario-StreamBe"><i className="bi bi-github"></i></a>
-                {message}
-                <a href="/Sobre-nosotros" >Sobre nosotros</a>
+            <nav className="navbar navbar-expand-lg justify-content-between footers px-4">
+                    <a href="https://github.com/Ramon-Ariel-ET12/Recetario-StreamBe" className="footer-icon"><i className="bi bi-github"></i></a>
+                    {message && <div className="footer-message">{message}</div>}
+                    <a href="/Sobre-nosotros" className="footer-link">Sobre nosotros</a>
             </nav>
         </footer>
     );
