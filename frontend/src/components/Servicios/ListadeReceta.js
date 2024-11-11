4c0ef2bb80b4f9c7964e9bb52dbc721685956bc4
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-11-10 21:02:24 -0300

    Un pequeño orden, falta mejorar

--- frontend/src/components/Servicios/ListadeReceta.js
+++ frontend/src/components/Servicios/ListadeReceta.js
@@ -27,32 +27,29 @@ export function Recetas() {
     }
 
     return (
-        <Card style={{ width: '18rem' }}>
-
+        <div className="row">
             {recetas.map((receta) => (
-                <div key={receta.idReceta}>
-                    {receta.imagen && receta.imagen.length > 0 ? (
-
-                        receta.imagen.map((imagen, cell) => (
-                            <div key={imagen.idImagen} style={{ height: '20vh', width: '20vh' }}>
-                                <img
-                                    src={`data:${imagen.formato};base64,${imagen.datos}`}
-                                    alt={`Receta ${cell + 1}`}
-                                    className="card-img-top"
-                                    style={{ width: '100%', height: '100%' }}
-                                    onError={(e) => { e.target.src = ''; e.target.style.backgroundColor = '#d3d3d3'; }}
-                                />
-                            </div>
-                        ))) : (<p>Error en obtener la imagen</p>)}
-
-                    <h5 >{receta.nombre}</h5>
-                    <p>{receta.descripcion}</p>
-                    <a href="." className="btn">Ver más</a>
-
+                <div className="col mb-4" key={receta.idReceta}>
+                    <Card style={{ width: '400px', height:'400px' }}>
+                        {receta.imagen && receta.imagen.length > 0 ? (
+                            receta.imagen.map((imagen, cell) => (
+                                <div key={imagen.idImagen} style={{ height: '100%', width: '100%' }}>
+                                    <img
+                                        src={`data:${imagen.formato};base64,${imagen.datos}`}
+                                        alt={`Receta ${cell + 1}`}
+                                        className="card-img-top"
+                                        style={{ width: '200px', height: '200px' }}
+                                        onError={(e) => { e.target.src = ''; e.target.style.backgroundColor = '#d3d3d3'; }}
+                                    />
+                                </div>
+                            ))) : (<p>Error en obtener la imagen</p>)}
+                        <h5>{receta.nombre}</h5>
+                        <p>{receta.descripcion}</p>
+                        <a href="." className="btn">Ver más</a>
+                    </Card>
                 </div>
             ))}
-
-        </Card>
+        </div>
     );
 }
 
--- frontend/src/components/Views/Inicio.js
+++ frontend/src/components/Views/Inicio.js
@@ -6,33 +6,35 @@ import Recetas from '../Servicios/ListadeReceta'
 
 const Inicio = () => {
   return (
-    <div>
-      <Card style={{ width: '90%', margin: '2rem auto', padding: '1.5rem', lineHeight: '1.6' }}>
-          <div className="card mb-3" style={{ maxWidth: '100%', borderStyle:'none' }}>
+    <div className='container' style={{maxWidth:'90%'}}>
+      <Card style={{ width: '100%', margin: '2rem auto', padding: '1.5rem', lineHeight: '1.6' }}>
+        <div className="card mb-3" style={{ maxWidth: '100%', borderStyle: 'none' }}>
           <div className="row g-0">
             <div className="col-md-8">
-              <div className="card-body" style={{color: 'white'}}>
+              <div className="card-body" style={{ color: 'white' }}>
                 <h5 className="card-title">Recetario</h5>
                 <p className="card-text ">
-                Es una plataforma colaborativa donde los usuarios pueden compartir y descubrir recetas caseras de la comunidad. Con una interfaz amigable, permite explorar recetas por categorías y niveles de dificultad. Cada receta refleja la creatividad de sus creadores, haciendo de Recetario un espacio auténtico y accesible para todos los amantes de la cocina.
+                  Es una plataforma colaborativa donde los usuarios pueden compartir y descubrir recetas caseras de la comunidad. Con una interfaz amigable, permite explorar recetas por categorías y niveles de dificultad. Cada receta refleja la creatividad de sus creadores, haciendo de Recetario un espacio auténtico y accesible para todos los amantes de la cocina.
                 </p>
               </div>
             </div>
             <div className="col-md-4">
-                <ReactPlayer
-                  url="https://youtu.be/AhaxAbtuy_E"
-                  width={'100%'}
-                  height={'250px'}
-                  controls
-                />
+              <ReactPlayer
+                url="https://youtu.be/AhaxAbtuy_E"
+                width={'100%'}
+                height={'250px'}
+                controls
+              />
             </div>
           </div>
         </div>
       </Card>
-      <Card style={{ width: '90%', margin: '2rem auto', padding: '1.5rem', lineHeight: '1.6' }}>
+      <Card style={{ width: '100%', margin: '2rem auto', padding: '1.5rem', lineHeight: '1.6' }}>
         <SearchBar />
       </Card>
-      <Recetas />
+
+        <Recetas />
+
     </div>
   )
 }
